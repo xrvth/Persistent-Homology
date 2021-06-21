d67f5399ebc1e13492a69ea41ea69675eb5d2675
Julia Ziemba <40124487+xrvth@users.noreply.github.com>
2021-06-21 17:13:41 +0200

    + explanations

--- main/project.py
+++ main/project.py
@@ -6,20 +6,20 @@ import matplotlib.pyplot as plt
 import gudhi
 
 def PlotBarcode(PointCloud):
-	RipsComplex = gudhi.RipsComplex(points=PointCloud, max_edge_length=0.8)
-	SimplexTree = RipsComplex.create_simplex_tree(max_dimension=3)
-	Diagram = SimplexTree.persistence(min_persistence=0.0)
+	RipsComplex = gudhi.RipsComplex(points=PointCloud, max_edge_length=0.8) #Creates a Rips complex, edge length is computed from a point cloud with a given distance function
+	SimplexTree = RipsComplex.create_simplex_tree(max_dimension=1) #Creates a simplex tree from the Rips complex, encoding the Vietoris–Rips filtration
+	Diagram = SimplexTree.persistence(min_persistence=0.0) #Returns persistence of the simplicial complex
 	return Diagram
 
-#Santoribiru Park (Kumamoto Prefecture, ~1000 points)
+#Santoribiru Park (Kumamoto Prefecture, ~1000 points, from OpenTopography)
 SantoribiruPark = laspy.read("../point-clouds/santoribiru_park.laz")
 Santoribiru = np.vstack([SantoribiruPark.X, SantoribiruPark.Y, SantoribiruPark.Z]).transpose()
-diagram = ripser(Santoribiru)['dgms']
+diagram = ripser(Santoribiru)['dgms'] #Returns the persistence diagrams values (['dgms'])
 plot_diagrams(diagram, show=True, title="Persistent Homology of Santoribiru Park Topography") 
 #bar = gudhi.plot_persistence_barcode(PlotBarcode(Santoribiru))
 #bar.set_title("Persistence Barcode of Santoribiru Park Topography")
 #plt.show()
-#UserWarning: Attempting to set identical left == right == 0.0 results in singular transformations; automatically expanding.
+#Disabled because of UserWarning: Attempting to set identical left == right == 0.0 results in singular transformations; automatically expanding.
 	#axes.axis([axis_start, infinity, 0, ind])
 
 #Cyclooctane
@@ -30,14 +30,6 @@ bar = gudhi.plot_persistence_barcode(PlotBarcode(Cyclooctane))
 bar.set_title("Persistence Barcode of Cyclooctane")
 plt.show()
 
-#Bunny
-#Bun = pye57.E57("../point-clouds/bunnyInt19.e57")
-#Bunny = np.vstack([Bun["cartesianX"], Bun["cartesianY"]]).transpose()
-#diagram = ripser(Bunny)['dgms']
-#plot_diagrams(diagram, show=True)
-#gudhi.plot_persistence_barcode(PlotBarcode(Bunny))
-#plt.show()
-
 #House
 House = np.loadtxt("../point-clouds/house.txt", delimiter=',')
 diagram = ripser(House)['dgms']
@@ -45,7 +37,7 @@ plot_diagrams(diagram, show=True, title="Persistent Homology of 5 points on a pl
 #bar = gudhi.plot_persistence_barcode(PlotBarcode(House))
 #bar.set_title("Persistence Barcode of 5 points on a plane")
 #plt.show()
-#UserWarning: Attempting to set identical left == right == 0.0 results in singular transformations; automatically expanding.
+#Disabled because of UserWarning: Attempting to set identical left == right == 0.0 results in singular transformations; automatically expanding.
 	#axes.axis([axis_start, infinity, 0, ind])
 
 #Torus
@@ -74,11 +66,22 @@ plt.show()
 
 #Cube
 Cube = np.loadtxt("../point-clouds/cube.txt", delimiter=' ')
+#diagram = ripser(Cube)
+#print(diagram)
 diagram = ripser(Cube)['dgms']
 plot_diagrams(diagram, show=True, title="Persistent Homology of Cube")
 bar = gudhi.plot_persistence_barcode(PlotBarcode(Cube))
 bar.set_title("Persistence Barcode of Cube")
 plt.show()
+
+
+#Bunny - disabled because of pye57 library not working
+#Bun = pye57.E57("../point-clouds/bunnyInt19.e57")
+#Bunny = np.vstack([Bun["cartesianX"], Bun["cartesianY"]]).transpose()
+#diagram = ripser(Bunny)['dgms']
+#plot_diagrams(diagram, show=True)
+#gudhi.plot_persistence_barcode(PlotBarcode(Bunny))
+#plt.show()
 
 
 # Datasets below unfortunately have too many points to process
